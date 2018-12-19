b3833f67f4ab8c842d563e42587be9272eef221d
unknown <ruben.de.neckere@student.ehb.be>
2018-12-19 11:08:40 +0100

    Initial commit

---
+++ app/src/main/java/werkcollege/ehb/ruben/androidwerkstuk/MainActivity.java
@@ -0,0 +1,13 @@
+package werkcollege.ehb.ruben.androidwerkstuk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
